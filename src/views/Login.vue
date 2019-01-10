307e92af3e16afb1aa476d1cab5a4e377defcb6a
shitong <284752471@qq.com>
2019-01-10 11:19:26 +0800

    初始化框架

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+        登录
+        <p>111</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .login{
+        p{
+            text-align: center;
+        }
+    }
+</style>
